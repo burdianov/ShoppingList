283040223b649e0aba29a4b769e31428bb409868
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-22 22:42:06 +0400

    Add the RecyclerView.Adapter

--- app/src/main/java/com/testography/shoppinglist/MainActivity.java
+++ app/src/main/java/com/testography/shoppinglist/MainActivity.java
@@ -8,10 +8,28 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mShoppingItems;
+
+    private RecyclerView.Adapter mShoppingItemsAdapter = new RecyclerView.Adapter() {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
