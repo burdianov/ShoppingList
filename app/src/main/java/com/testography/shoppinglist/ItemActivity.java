fb44bf9f7de1ab19f4e65034d56e55976c0f4e3d
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-22 21:30:42 +0400

    Inflate the menu

--- app/src/main/java/com/testography/shoppinglist/ItemActivity.java
+++ app/src/main/java/com/testography/shoppinglist/ItemActivity.java
@@ -2,6 +2,8 @@ package com.testography.shoppinglist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ItemActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class ItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.save_action) {
+            //TO DO
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
