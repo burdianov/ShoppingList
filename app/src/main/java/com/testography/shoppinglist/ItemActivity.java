9a269f1f4ba454e574005c6830d956f0df7d0402
Leo Burdianov <leo.burdianov@gmail.com>
2016-08-22 23:41:41 +0400

    Add shopping items

--- app/src/main/java/com/testography/shoppinglist/ItemActivity.java
+++ app/src/main/java/com/testography/shoppinglist/ItemActivity.java
@@ -4,8 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.UUID;
+
+import io.realm.Realm;
 
 public class ItemActivity extends AppCompatActivity {
+
+    private EditText mInputItemName;
+    private EditText mInputItemQuantity;
+    private Realm mRealm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,11 @@ public class ItemActivity extends AppCompatActivity {
         if (getIntent().hasExtra("TITLE")) {
             setTitle(getIntent().getStringExtra("TITLE"));
         }
+
+        mInputItemName = (EditText) findViewById(R.id.input_item_name);
+        mInputItemQuantity = (EditText) findViewById(R.id.input_item_quantity);
+
+        mRealm = Realm.getDefaultInstance();
     }
 
     @Override
@@ -26,8 +40,22 @@ public class ItemActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.save_action) {
-            //TO DO
+
+            mRealm.beginTransaction();
+
+            ShoppingItem shoppingItem = mRealm.createObject(ShoppingItem.class);
+
+            shoppingItem.setName(mInputItemName.getText().toString());
+            shoppingItem.setQuantity(mInputItemQuantity.getText().toString());
+            shoppingItem.setCompleted(false);
+            shoppingItem.setId(UUID.randomUUID().toString());
+            shoppingItem.setTimestamp(System.currentTimeMillis());
+
+            mRealm.commitTransaction();
+
+            finish();
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
